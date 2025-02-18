4b2998c15615a7582207c31e3199f323c9138d76
Nivrutti Shelar <nivruttishelar445@gmail.com>
2025-02-18 15:46:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/K Closest Points to Origin/k-closest-points-to-origin.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        // Your code here
+        
+        priority_queue<pair<int,vector<int>>> pq;
+        
+        for(int i=0;i<points.size();i++)
+        {
+            int a=points[i][0];
+            int b=points[i][1];
+            
+            int val=a*a+b*b;
+            
+            vector<int> arr={a,b};
+            
+            pair<int,vector<int>> p={val, arr};
+            
+            pq.push(p);
+            if(pq.size()>k)
+            pq.pop();
+            
+            
+        }
+        vector<vector<int>> ans;
+        
+        while(pq.size())
+        {
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int k;
+        cin >> k;
+        int n;
+        cin >> n;
+        vector<vector<int>> points(n, vector<int>(2));
+        for (int i = 0; i < n; i++) {
+            cin >> points[i][0] >> points[i][1];
+        }
+        Solution ob;
+        vector<vector<int>> ans = ob.kClosest(points, k);
+        sort(ans.begin(), ans.end());
+        for (const vector<int>& point : ans) {
+            cout << point[0] << " " << point[1] << endl;
+        }
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
