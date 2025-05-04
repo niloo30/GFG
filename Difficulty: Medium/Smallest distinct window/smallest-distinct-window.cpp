910b353490aa229470b6d06bd01bebfecaccb30b
Nivrutti Shelar <nivruttishelar445@gmail.com>
2025-05-04 23:10:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Smallest distinct window/smallest-distinct-window.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    int findSubString(string& str) {
+        // code here
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+//      Driver code
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+
+        string str;
+        cin >> str;
+        Solution ob;
+        cout << ob.findSubString(str) << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
