a7c3cabf8eadac77de0f6b6793ef61c526ba047e
Nivrutti Shelar <nivruttishelar445@gmail.com>
2025-07-24 15:15:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
+++ Difficulty: Easy/DFS of Graph/dfs-of-graph.cpp
@@ -1,72 +1,35 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
     vector<int> ans;
-    void f(vector<vector<int>>& adj,int val,int vis[])
+    void dfs(int ele ,vector<int>& vis,vector<vector<int>>& adj)
     {
-        ans.push_back(val);
-        vis[val]=1;
+        ans.push_back(ele);
+        vis[ele]=1;
         
-        for(auto ele:adj[val])
+        for(auto it:adj[ele])
         {
-            if(vis[ele]==0)
-            {
-                f(adj,ele,vis);
-            }
+            if(!vis[it])
+            dfs(it,vis,adj);
         }
     }
     vector<int> dfs(vector<vector<int>>& adj) {
         // Code here
+        
         int n=adj.size();
-        int vis[n]={0};//not visited till now
+        vector<int> vis(n,0);
         
-        f(adj,0,vis);
+        ans.push_back(0);
+        vis[0]=1;
         
+        for(auto ele: adj[0])
+        {
+            if(!vis[ele])
+            {
+                dfs(ele,vis,adj);
+            }
+        }
         return ans;
+        
+        
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int tc;
-    cin >> tc;
-    cin.ignore();
-    while (tc--) {
-        int V;
-        cin >> V;
-        cin.ignore();
-        vector<vector<int>> adj(
-            V); // Use vector of vectors instead of array of vectors.
-
-        for (int i = 0; i < V; i++) {
-            string input;
-            getline(cin, input);
-            int num;
-            vector<int> node;
-            stringstream ss(input);
-            while (ss >> num) {
-                node.push_back(num);
-            }
-            adj[i] = node;
-        }
-
-        Solution obj;
-        vector<int> ans = obj.dfs(adj);
-        for (int i = 0; i < ans.size(); i++) {
-            cout << ans[i] << " ";
-        }
-        cout << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
