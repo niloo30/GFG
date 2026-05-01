de2e82fc5ed3e7b12d6a947a890bff0979d08d48
Nivrutti Shelar <nivruttishelar445@gmail.com>
2026-05-01 11:21:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/LCM And GCD/lcm-and-gcd.cpp
+++ Difficulty: Basic/LCM And GCD/lcm-and-gcd.cpp
@@ -2,11 +2,10 @@ class Solution {
   public:
     vector<int> lcmAndGcd(int a, int b) {
         // code here
-        
         int g=__gcd(a,b);
         int l=(a*b)/g;
         
-        return {l,g};
         
+        return {l,g};
     }
 };
