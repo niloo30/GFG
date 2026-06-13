93893a30b0e3899c6b7de73b3c9cbbf1502ccca1
Nivrutti Shelar <nivruttishelar445@gmail.com>
2026-06-13 11:13:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/First negative in every window of size k/first-negative-in-every-window-of-size-k.cpp
+++ Difficulty: Medium/First negative in every window of size k/first-negative-in-every-window-of-size-k.cpp
@@ -1,41 +1,28 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
     vector<int> firstNegInt(vector<int>& arr, int k) {
         // write code here
-        
         int n=arr.size();
-        
-        vector<int> ans;
         
         queue<int> q;
         
+        vector<int> ans;
         int i=0,j=0;
         
-        while(j<n)
-        {
-            if(arr[j]<0)
-            q.push(j);
+        while(j<n){
             
-            if(j-i+1==k)
-            {
+            if(arr[j]<0)
+            q.push(arr[j]);
+            
+            if(j-i+1==k){
                 if(q.empty())
                 ans.push_back(0);
                 else
-                ans.push_back(arr[q.front()]);
+                ans.push_back(q.front());
                 
-                if(q.size() and i==q.front())
-                {
-                    
-                    q.pop();
-                }
                 
+                if(arr[i]<0)
+                q.pop();
                 i++;
             }
             
@@ -45,34 +32,3 @@ class Solution {
         return ans;
     }
 };
-
-
-//{ Driver Code Starts.
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-    while (t--) {
-
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        string ks;
-        getline(cin, ks);
-        int k = stoi(ks);
-        Solution ob;
-        vector<int> ans = ob.firstNegInt(arr, k);
-        for (auto it : ans) {
-            cout << it << " ";
-        }
-        cout << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
