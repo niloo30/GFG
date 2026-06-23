9d346f97d391b98556db80f20284a050664461b8
Nivrutti Shelar <nivruttishelar445@gmail.com>
2026-06-23 19:18:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -1,51 +1,63 @@
 class Solution {
   public:
+    bool detect(int src,vector<int>& vis,vector<vector<int>>& graph)
+    {
+        queue<pair<int,int>> q; //{ele,parent}
+        
+        
+        q.push({src,-1});
+        vis[src]=1;
+        
+        while(!q.empty())
+        {
+            int node=q.front().first;
+            int parent=q.front().second;
+            
+            q.pop();
+            
+            for(auto ele:graph[node])
+            {
+                if(vis[ele]==0)
+                {
+                    q.push({ele,node});
+                    vis[ele]=1;
+                }
+                else //vis[ele]==1
+                {
+                    if(parent!=ele)
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
     bool isCycle(int V, vector<vector<int>>& edges) {
         // Code here
+        vector<vector<int>> graph(V);
         
-        vector<vector<int>> adj(V);
-        
-        for(auto ele: edges)
+        for(int i=0;i<edges.size();i++)
         {
-            int u=ele[0];
-            int v=ele[1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
+            int u=edges[i][0];
+            int v=edges[i][1];
+            
+            graph[u].push_back(v);
+            graph[v].push_back(u);
         }
         
         vector<int> vis(V,0);
+        
         for(int i=0;i<V;i++)
         {
             if(!vis[i])
             {
-                queue<pair<int,int>> q;
-                q.push({i,-1}); // ele, parent->from which node it came from 
-                vis[i]=1;
-        
-                while(!q.empty())
-                {
-                    int ele=q.front().first;
-                    int parent=q.front().second;
-                    
-                    q.pop();
-                    
-                    for(auto it: adj[ele])
-                    {
-                        if(it!=parent)
-                        {
-                            if(vis[it])
-                            return true;
-                            
-                            vis[it]=1;
-                            q.push({it,ele});
-                        }
-                    }
-                }
-                
+                if(detect(i,vis,graph))
+                return true;
             }
-            
         }
         
         return false;
+        
+        
     }
 };
