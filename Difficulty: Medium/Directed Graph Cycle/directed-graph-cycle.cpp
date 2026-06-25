c172edee8c7224290ac182be8f9b4c08bd1213a1
Nivrutti Shelar <nivruttishelar445@gmail.com>
2026-06-25 15:51:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -0,0 +1,52 @@
+class Solution {
+  public:
+    bool dfs(int src,vector<int>& vis,vector<int>& pathvis,vector<vector<int>>& graph)
+    {
+        vis[src]=1;
+        pathvis[src]=1;
+        
+        
+        for(auto ele:graph[src])
+        {
+            if(!vis[ele])
+            {
+                if(dfs(ele,vis,pathvis,graph))
+                return true;
+            }
+            else if(pathvis[ele]==1)
+            return true;
+        }
+        
+        pathvis[src]=0;
+        return false;
+    }
+    
+    bool isCyclic(int V, vector<vector<int>> &edges) {
+        // code here
+        
+        vector<vector<int>> graph(V);
+        
+        for(int i=0;i<edges.size();i++)
+        {
+            int u=edges[i][0];
+            int v=edges[i][1];
+            
+            graph[u].push_back(v);
+        }
+        
+        //graph is ready 
+        
+        vector<int> vis(V,0);
+        vector<int> pathvis(V,0);
+        
+        for(int i=0;i<V;i++)
+        {
+            if(!vis[i])
+            {
+                if(dfs(i,vis,pathvis,graph))
+                return true;
+            }
+        }
+        return false;
+    }
+};
